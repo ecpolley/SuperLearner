0a31807445a1594ac3692afe9632a9e5cfea4ba3
Eric Polley <eric.polley@nih.gov>
2011-05-02 03:45:22 -0400

    fixed screen.glmnet
    Signed-off-by: Eric Polley <eric.polley@nih.gov>

--- R/screen.glmnet.R
+++ R/screen.glmnet.R
@@ -2,19 +2,15 @@ screen.glmnet <- function(Y, X, family, alpha = 1, minscreen = 2, nfolds = 10, n
   .SL.require('glmnet')
   if(!is.matrix(X)) {
     X <- model.matrix(~ -1 + ., X)
-    newX <- model.matrix(~ -1 + ., newX)
   }
   fitCV <- cv.glmnet(x = X, y = Y, lambda = NULL, type.measure = 'deviance', nfolds = nfolds, family = family$family, alpha = alpha, nlambda = nlambda)
-  whichVariable <- (fit.first$beta[, which.min(cv.net.fit$cv)] != 0)
+  whichVariable <- (as.numeric(coef(fitCV$glmnet.fit, s = fitCV$lambda.min))[-1] != 0)
+  # the [-1] removes the intercept
   if (sum(whichVariable) < minscreen) {
       warning("fewer than minscreen variables passed the glmnet screen, increased lambda to allow minscreen variables")
-	sumCoef <- apply(fit.first$beta, 2, function(x) sum((x != 0)))
-	newCut <- which.max(sumCoef >= minscreen)
-	whichVariable <- (fit.first$beta[, newCut] != 0)
+	    sumCoef <- apply(as.matrix(fitCV$glmnet.fit$beta), 2, function(x) sum((x != 0)))
+	    newCut <- which.max(sumCoef >= minscreen)
+	    whichVariable <- (as.matrix(fitCV$glmnet.fit$beta)[, newCut] != 0)
   }
-  return(whichVariable)
-  # whichVariable is a logical vector,
-  # TRUE indicates variable will be used
-  whichVariable <- rep(TRUE, ncol(X))
   return(whichVariable)
 }
