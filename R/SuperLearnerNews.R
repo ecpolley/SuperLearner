ae5a3bbd68a66c6225c655d710765a9900c4de02
Eric Polley <eric.polley@nih.gov>
2011-05-23 08:42:20 -0400

    Added function SuperLearnerDocs() to be able to open the PDF files in the /doc folder (SuperLearnerR.pdf and SuperLearner.pdf)
    Signed-off-by: Eric Polley <eric.polley@nih.gov>

--- R/SuperLearnerNews.R
+++ R/SuperLearnerNews.R
@@ -1,3 +1,16 @@
 SuperLearnerNews <- function(...) {
 	RShowDoc("NEWS", package = "SuperLearner", ...)
 }
+
+SuperLearnerDocs <- function(what = 'SuperLearnerR.pdf', ...) {
+  if(what == 'NEWS') {
+    RShowDoc('NEWS', package = "SuperLearner", ...)
+  } else {
+    f <- system.file('doc', what, package = 'SuperLearner')
+    if (.Platform$OS.type == 'windows')
+     shell.exec(f)
+     else system(paste(Sys.getenv('R_PDFVIEWER'), f, '&'))
+     return(f)
+  }
+	
+}
