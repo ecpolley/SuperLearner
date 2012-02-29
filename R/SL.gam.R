d99e219b3e23653507f75f3e5a22f8043c063ebe
Eric Polley <eric.polley@nih.gov>
2012-02-29 13:56:22 -0500

    Updated News
    Signed-off-by: Eric Polley <eric.polley@nih.gov>

--- R/SL.gam.R
+++ R/SL.gam.R
@@ -6,6 +6,7 @@
 
 SL.gam <- function(Y, X, newX, family, obsWeights, deg.gam = 2, cts.num = 4, ...) {
   .SL.require('gam')
+  if("mgcv" %in% loadedNamespaces()) warning("mgcv and gam packages are both in use. You might see an error because both packages use the same function names.")
   # create the formula for gam with a spline for each continuous variable
   cts.x <- apply(X, 2, function(x) (length(unique(x)) > cts.num))
   if (sum(!cts.x) > 0) { 
@@ -17,8 +18,8 @@ SL.gam <- function(Y, X, newX, family, obsWeights, deg.gam = 2, cts.num = 4, ...
   if (sum(!cts.x) == length(cts.x)) {
     gam.model <- as.formula(paste("Y~", paste(colnames(X), collapse = "+"), sep = ""))
   }
-  fit.gam <- gam::gam(gam.model, data = X, family = family, control = gam.control(maxit = 50, bf.maxit = 50), weights = obsWeights)
-  pred <- predict(fit.gam, newdata = newX, type = "response")
+  fit.gam <- gam::gam(gam.model, data = X, family = family, control = gam::gam.control(maxit = 50, bf.maxit = 50), weights = obsWeights)
+  pred <- gam::predict.gam(fit.gam, newdata = newX, type = "response")
   fit <- list(object = fit.gam)
   out <- list(pred = pred, fit = fit)
   class(out$fit) <- c("SL.gam")
