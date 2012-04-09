27beca600c347117f339b6e5bd7e02651deb21cd
Eric Polley <eric.polley@nih.gov>
2012-04-09 10:52:47 -0400

    added onlySL argument to predict.SuperLearner. Helpful to speed up prediction if only need SL predictions and/or some of the library algorithms experienced errors.
    Signed-off-by: Eric Polley <eric.polley@nih.gov>

--- R/predict.SuperLearner.R
+++ R/predict.SuperLearner.R
@@ -1,4 +1,4 @@
-predict.SuperLearner <- function(object, newdata, X = NULL, Y = NULL, ...) {
+predict.SuperLearner <- function(object, newdata, X = NULL, Y = NULL, onlySL = FALSE, ...) {
   if(missing(newdata)) {
     out <- list(pred = object$SL.predict, library.predict = object$library.predict)
     return(out)
@@ -7,20 +7,37 @@ predict.SuperLearner <- function(object, newdata, X = NULL, Y = NULL, ...) {
   k <- length(object$libraryNames)
   predY <- matrix(NA, nrow = nrow(newdata), ncol = k)
   colnames(predY) <- object$libraryNames
-  for(mm in seq(k)) {
-    newdataMM <- subset(newdata, select = object$whichScreen[object$SL.library$library[mm, 2], ])
-    family <- object$family
-    XMM <- if(is.null(X)) NULL else subset(X, select = object$whichScreen[object$SL.library$library[mm, 2], ])
-    predY[, mm] <- do.call('predict', list(object = object$fitLibrary[[mm]], 
+  if(onlySL) {
+    whichLibrary <- which(object$coef > 0)
+    predY <- matrix(0, nrow = nrow(newdata), ncol = k)
+    for(mm in whichLibrary) {
+      newdataMM <- subset(newdata, select = object$whichScreen[object$SL.library$library[mm, 2], ])
+      family <- object$family
+      XMM <- if(is.null(X)) NULL else subset(X, select = object$whichScreen[object$SL.library$library[mm, 2], ])
+      predY[, mm] <- do.call('predict', list(object = object$fitLibrary[[mm]], 
                                     newdata = newdataMM,
                                     family = family,
                                     X = XMM,
                                     Y = Y,
                                     ...))
+      }
+    getPred <- object$method$computePred(predY = predY, coef = object$coef, control = object$control)
+    out <- list(pred = getPred, library.predict = predY)
+  } else {
+    for(mm in seq(k)) {
+      newdataMM <- subset(newdata, select = object$whichScreen[object$SL.library$library[mm, 2], ])
+      family <- object$family
+      XMM <- if(is.null(X)) NULL else subset(X, select = object$whichScreen[object$SL.library$library[mm, 2], ])
+      predY[, mm] <- do.call('predict', list(object = object$fitLibrary[[mm]], 
+                                    newdata = newdataMM,
+                                    family = family,
+                                    X = XMM,
+                                    Y = Y,
+                                    ...))
+      }
+    getPred <- object$method$computePred(predY = predY, coef = object$coef, control = object$control)
+    out <- list(pred = getPred, library.predict = predY)
   }
-
-  getPred <- object$method$computePred(predY = predY, coef = object$coef, control = object$control)
-  out <- list(pred = getPred, library.predict = predY)
   return(out)
 }
 
