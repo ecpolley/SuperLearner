f0f0b7e43c5a9e620ba438676aa6c5811bb3034a
ledell <ledell@stat.berkeley.edu>
2015-04-25 21:17:55 -0700

    Added SL.nnls wrapper

---
+++ R/SL.nnls.R
@@ -0,0 +1,34 @@
+# SL-wrapper for Non-negative least squares algorithm
+# Same functionality as the method.NNLS metalearner, but 
+# in the SL-wrapper format
+
+SL.nnls <- function(Y, X, newX, family, obsWeights, ...) {
+  .SL.require("nnls")
+  fit.nnls <- nnls(sqrt(obsWeights)*as.matrix(X), sqrt(obsWeights)*Y) 
+  initCoef <- coef(fit.nnls)
+  initCoef[is.na(initCoef)] <- 0
+  if (sum(initCoef) > 0) {
+    coef <- initCoef/sum(initCoef)
+  } else {
+    warning("All algorithms have zero weight", call. = FALSE)
+    coef <- initCoef
+  }
+  pred <- crossprod(t(as.matrix(newX)), coef)
+  fit <- list(object = fit.nnls)
+  class(fit) <- "SL.nnls"
+  out <- list(pred = pred, fit = fit)
+  return(out)
+}
+
+predict.SL.nnls <- function(object, newdata, ...) {
+  initCoef <- coef(object$object)
+  initCoef[is.na(initCoef)] <- 0
+  if (sum(initCoef) > 0) {
+    coef <- initCoef/sum(initCoef)
+  } else {
+    warning("All algorithms have zero weight", call. = FALSE)
+    coef <- initCoef
+  }
+  pred <- crossprod(t(as.matrix(newdata)), coef)
+  return(pred)
+}
