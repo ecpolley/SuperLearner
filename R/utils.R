f4aa2b9f475367225667baa87c5551ecb03d7397
Can Candan <cancandan@gmail.com>
2015-07-28 11:36:26 +0300

    mergeSuperLearners fix

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,61 @@
 # merge two superlearners and refit
 # Usage:
 # Create a SuperLearner.CV.control with validRows given
-# Fit two SuperLearners providing the same SuperLearner.CV.control
-# Call mergeSuperLearners(oldsl,newsl,Y,method)
+# Fit two SuperLearners with the same SuperLearner.CV.control
+# Call mergeSuperLearners(firstsl,secondsl,Y,method)
+
+# ## simulate data
+# set.seed(23432)
+# ## training set
+# n <- 500
+# p <- 50
+# X <- matrix(rnorm(n*p), nrow = n, ncol = p)
+# colnames(X) <- paste("X", 1:p, sep="")
+# X <- data.frame(X)
+# Y <- X[, 1] + sqrt(abs(X[, 2] * X[, 3])) + X[, 2] - X[, 3] + rnorm(n)
+# 
+# ## test set
+# m <- 1000
+# newX <- matrix(rnorm(m*p), nrow = m, ncol = p)
+# colnames(newX) <- paste("X", 1:p, sep="")
+# newX <- data.frame(newX)
+# newY <- newX[, 1] + sqrt(abs(newX[, 2] * newX[, 3])) + newX[, 2] -
+#   newX[, 3] + rnorm(m)
+# 
+# # generate Library and run Super Learner
+# SL.library <- c("SL.glm", "SL.mean")
+# test1 <- SuperLearner(Y = Y, X = X, newX = newX, SL.library = SL.library,
+#                      verbose = TRUE, method = "method.NNLS")
+# test1
+# 
+# SL.library <- c("SL.glmnet", "SL.loess")
+# 
+# ctrl=SuperLearner.CV.control(V = 10,validRows = test$validRows)
+# test2 <- SuperLearner(Y = Y, X = X, newX = newX, SL.library = SL.library,cvControl = ctrl,
+#                      verbose = TRUE, method = "method.NNLS")
+# test2
+# merged=mergeSuperLearners(test,test2,Y = Y,X=X)
+# merged
 
 
-mergeSuperLearners <-function(oldsl,newsl,Y,method='method.NNLS',verbose=T){  
-  oldsl$Z=cbind(oldsl$Z,newsl$Z)
-  oldsl$libraryNames=c(oldsl$libraryNames,newsl$libraryNames)
-  oldsl$library.predict=cbind(oldsl$library.predict,newsl$library.predict)
-  oldsl$SL.library$library=rbind(oldsl$SL.library$library,newsl$SL.library$library)
-  oldsl$errorsInLibrary=c(oldsl$errorsInLibrary,newsl$errorsInLibrary)
-  oldsl$errorsInCVLibrary=c(oldsl$errorsInLibrary,newsl$errorsInLibrary)
+mergeSuperLearners <-function(firstsl,secondsl,Y,X,method='method.NNLS',obsWeights=NULL,verbose=T){    
+  # combine the Z matrices, libraryNames, predictions, errors
+  firstsl$Z=cbind(firstsl$Z,secondsl$Z)
+  firstsl$libraryNames=c(firstsl$libraryNames,secondsl$libraryNames)
+  firstsl$library.predict=cbind(firstsl$library.predict,secondsl$library.predict)
+  firstsl$SL.library$library=rbind(firstsl$SL.library$library,secondsl$SL.library$library)
+  firstsl$errorsInLibrary=c(firstsl$errorsInLibrary,secondsl$errorsInLibrary)
+  firstsl$errorsInCVLibrary=c(firstsl$errorsInLibrary,secondsl$errorsInLibrary)
   
-  object=oldsl
+  N <- dim(X)[1L]    
+  if(is.null(obsWeights)) {
+    obsWeights <- rep(1, N)
+  }
+  if(!identical(length(obsWeights), N)) {
+    stop("obsWeights vector must have the same dimension as Y")
+  }
+  
+  object=firstsl
   if(is.character(method)) {
     if(exists(method, mode = 'list')) {
       method <- get(method, mode = 'list')
@@ -29,18 +71,18 @@ mergeSuperLearners <-function(oldsl,newsl,Y,method='method.NNLS',verbose=T){
   if(!is.null(method$require)) {
     sapply(method$require, function(x) require(force(x), character.only = TRUE))
   }
-
+  
   
   # Compute new coefficients for the combined SuperLearner
-  getCoef <- method$computeCoef(Z = object$Z, Y = Y , libraryNames = object$libraryNames, obsWeights = object$obsWeights, control = object$control, verbose = verbose)
+  getCoef <- method$computeCoef(Z = object$Z, Y = Y , libraryNames = object$libraryNames, obsWeights = obsWeights, control = object$control, verbose = verbose)
   coef <- getCoef$coef
   names(coef) <- object$libraryNames
   getPred <- method$computePred(predY = object$library.predict, coef = coef, control = object$control)
-    
+  
   if(sum(object$errorsInCVLibrary) > 0) {
     getCoef$cvRisk[as.logical(object$errorsInCVLibrary)] <- NA
   }
-    
+  
   object$SL.predict=getPred
   object$coef=coef    
   object$cvRisk = getCoef$cvRisk
