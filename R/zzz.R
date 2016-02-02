3b83488f167a2a4016fc0ff9a6b09fc55ba7628e
Eric Polley <ecpolley@gmail.com>
2016-02-02 14:48:33 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   packageStartupMessage('Super Learner')
   packageStartupMessage('Version: ', utils::packageDescription('SuperLearner')$Version)
   packageStartupMessage('Package created on ', utils::packageDescription('SuperLearner')$Date, '\n')
-  packageStartupMessage('Use SuperLearnerNews() to see changes from previous versions and latest news', '\n')
+  # packageStartupMessage('Use SuperLearnerNews() to see changes from previous versions and latest news', '\n')
   # packageStartupMessage('Suggested packages to install for the Super Learner library:')
   # packageStartupMessage(utils::packageDescription('SuperLearner')$Suggests)
 }
