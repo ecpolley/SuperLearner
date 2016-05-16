9706346c743957c67d3391c5e85f255397f83201
Eric Polley <ecpolley@gmail.com>
2016-05-16 14:12:33 -0500

    Update method.R
    Updated method.AUC with "L-BFGS-B" and [0, inf) bounds as default. Also includes warning message if non-convergence

--- R/method.R
+++ R/method.R
@@ -247,35 +247,145 @@ method.CC_nloglik <- function() {
        computeCoef=computeCoef,
        computePred=computePred)
 }
-method.AUC <- function(optim_method="Nelder-Mead") {
-	# Contributed by Erin Ledell
-	out <- list(
-		require = c('cvAUC', 'ROCR'),
-		computeCoef = function(Z, Y, libraryNames, obsWeights, control, verbose, ...) {
-			.cvRisk_AUC <- function(par, Z, Y, folds=NULL){
-				# Calculate cvRisk, which is 1-cvAUC (Rank Loss)
-				# This is the loss function that gets fed into optim as the "fn" argument 
-				# par is the weight/coef vector for the ensemble in Super Learner
-				predictions <- crossprod(t(Z), par)  #cv predicted SL values
-				cvRisk <- 1 - cvAUC::cvAUC(predictions=predictions, labels=Y, folds=folds)$cvAUC
-				return(cvRisk)
-			}
-			coef_init <- rep(1/ncol(Z),ncol(Z))
-			names(coef_init) <- libraryNames
-			# optim function selects the value for par that minimizes cvRisk_AUC (aka Rank Loss)
-			res <- optim(par=coef_init, fn=.cvRisk_AUC, Z=Z, Y=Y, folds=NULL, method=optim_method)
-			if(res$convergence != 0) warning(paste("optim didn't converge when estimating the super learner coefficients: ", res$convergence))
-			coef <- res$par
-			auc <- apply(Z, 2, function(x) cvAUC::cvAUC(x, labels=Y)$cvAUC)
-			cvRisk <- 1 - auc  # Rank Loss
-			names(coef) <- libraryNames
-			out <- list(cvRisk = cvRisk, coef = coef)
-			return(out)
-		},
-		computePred = function(predY, coef, control, ...) {
-			out <- crossprod(t(predY), coef)
-			return(out)
-		}
-	)
-	invisible(out)
+method.AUC <- function(nlopt_method = NULL, optim_method = "L-BFGS-B", 
+                       bounds = c(0, Inf), normalize = TRUE) {
+  # Contributed by Erin LeDell
+  if (!is.null(nlopt_method) && !is.null(optim_method)) {
+    stop("Please supply either a nlopt or optim method; one of these must be set to NULL.")
+  }
+  
+  #if (length(optim_method) > 0) {
+  #  if (length(intersect(optim_method, c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN"))) == 0) {
+  if (!is.null(optim_method)) {
+    if (!(optim_method %in% c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN"))) {
+      stop("supplied 'optim_method' value not supported")
+    } 
+    out <- list(
+      require = 'cvAUC',
+      # computeCoef is a function that returns a list with two elements:
+      # 1) coef: the weights (coefficients) for each algorithm 
+      # 2) cvRisk: the V-fold CV risk for each algorithm
+      computeCoef = function(Z, Y, libraryNames, obsWeights, control, verbose, ...) {
+        .cvRisk_AUC <- function(par, Z, Y, folds = NULL) {
+          # Calculate cv Risk, which is 1-cvAUC (rank loss);
+          # This is the general loss function that gets fed into optim as the "fn" argument 
+          # par is the weight/coef vector for ensemble in Super Learner
+          predictions <- crossprod(t(Z), par)  #cv predicted SL values
+          # Now calculate cv risk (this is what we want to minimize)
+          # Might change this to AUC only since we are currently not using folds arg...
+          cvRisk <- 1 - cvAUC(predictions = predictions, labels = Y, folds = folds)$cvAUC
+          return(cvRisk)
+        }
+        coef_init <- rep(1/ncol(Z),ncol(Z))
+        names(coef_init) <- libraryNames
+        # optim function selects the value for par that minimizes .cvRisk_AUC (ie. rank loss)
+        res <- optim(par = coef_init, 
+                     fn = .cvRisk_AUC, 
+                     Z = Z, 
+                     Y = Y, 
+                     folds = NULL, 
+                     method = optim_method, 
+                     lower = bounds[1], 
+                     upper = bounds[2])
+		if(res$convergence != 0) warning(paste("optim didn't converge when estimating the super learner coefficients, reason (see ?optim): ", res$convergence))
+        coef <- res$par
+        if (any(is.na(coef))) {
+          warning("Some algorithms have weights of NA, setting to 0.")
+          coef[is.na(coef)] <- 0 
+        }
+        if (!sum(coef) > 0) warning("All algorithms have zero weight", call. = FALSE)
+        if (normalize) coef <- coef/sum(coef)
+        #print(coef)
+        auc <- apply(Z, 2, function(x) AUC(predictions = x, labels = Y))
+        # If we update the getCoef function in SL to include 'folds' we can use the below auc instead
+        # auc <- apply(Z, 2, function(x) cvAUC(x, labels=Y, folds=validRows)$cvAUC)
+        cvRisk <- 1 - auc  # rank loss
+        names(coef) <- libraryNames
+        out <- list(cvRisk = cvRisk, coef = coef)
+        return(out)
+      },
+      # computePred is a function that takes the weights and the predicted values from each algorithm in the library and combines them based on the model to output the super learner predicted values
+      computePred = function(predY, coef, control, ...) {
+        out <- crossprod(t(predY), coef)
+        return(out)
+      }
+    )    
+#  } else if (length(nlopt_method) > 0) {
+  } else if (!is.null(nlopt_method)) {
+    nlopt_global <- c("NLOPT_GN_DIRECT", 
+                             "NLOPT_GN_DIRECT_L", 
+                             "NLOPT_GN_DIRECT_L_RAND", 
+                             "NLOPT_GN_DIRECT_NOSCAL",
+                             "NLOPT_GN_DIRECT_L_NOSCAL",
+                             "NLOPT_GN_DIRECT_L_RAND_NOSCAL",
+                             "NLOPT_GN_ORIG_DIRECT",
+                             "NLOPT_GN_ORIG_DIRECT_L",
+                             "NLOPT_GN_CRS2_LM",
+                             "NLOPT_GN_ISRES")
+    nlopt_local <- c("NLOPT_LN_PRAXIS",
+                            "NLOPT_LN_COBYLA",  
+                            "NLOPT_LN_NEWUOA_BOUND", 
+                            "NLOPT_LN_NELDERMEAD", 
+                            "NLOPT_LN_SBPLX",
+                            "NLOPT_LN_BOBYQA")
+    #if (length(intersect(nlopt_method, c(nlopt_global, nlopt_local))) == 0) {
+    if (!(nlopt_method %in% c(nlopt_global, nlopt_local))) {
+      stop("supplied 'nlopt_method' value not supported")
+    } 
+    out <- list(
+      require = c('cvAUC', 'nloptr'),
+      # computeCoef is a function that returns a list with two elements:
+      # 1) coef: the weights (coefficients) for each algorithm 
+      # 2) cvRisk: the V-fold CV risk for each algorithm
+      computeCoef = function(Z, Y, libraryNames, obsWeights, control, verbose, ...) {
+        .cvRisk_AUC <- function(par, Z, Y){
+          # Calculate cv Risk, which is 1-cvAUC (rank loss);
+          # This is the general loss function that gets fed into optim as the "fn" argument 
+          # par is the weight/coef vector for ensemble in Super Learner
+          predictions <- crossprod(t(Z), par)  #cv predicted SL values
+          # Now calculate cv risk (this is what we want to minimize)
+          cvRisk <- 1 - cvAUC(predictions = predictions, labels = Y, folds = NULL)$cvAUC
+          return(cvRisk)
+        }
+        coef_init <- rep(1/ncol(Z), ncol(Z))
+        names(coef_init) <- libraryNames
+        # nloptr function selects the value for par that minimizes .cvRisk_AUC (ie. rank loss)
+        res <- nloptr(x0 = coef_init,
+                      eval_f = .cvRisk_AUC,
+                      lb = rep(bounds[1], ncol(Z)),
+                      ub = rep(bounds[2], ncol(Z)),
+                      #eval_g_ineq = .constraint_ineq,
+                      #eval_g_eq = .constraint_eq,
+                      opts = list(algorithm = nlopt_method, xtol_rel = 1e-08),
+                      Z = Z,
+                      Y = Y)
+        if (res$status < 1 || res$status > 4) {
+		  warning(res$message)
+		}			  
+        coef <- res$solution
+        if (any(is.na(coef))) {
+          warning("Some algorithms have weights of NA, setting to 0.")
+          coef[is.na(coef)] <- 0 
+        }
+        if (!sum(coef) > 0) warning("All algorithms have zero weight", call. = FALSE)
+        if (normalize) coef <- coef/sum(coef)
+        auc <- apply(Z, 2, function(x) AUC(predictions = x, labels = Y))
+        ## If we update the getCoef function in SL to include 'folds' we can use the below auc instead
+        ## auc <- apply(Z, 2, function(x) cvAUC(x, labels=Y, folds=validRows)$cvAUC)
+        cvRisk <- 1 - auc  # rank loss
+        names(coef) <- libraryNames
+        out <- list(cvRisk = cvRisk, coef = coef)
+        return(out)
+      },
+      # computePred is a function that takes the weights and the predicted values from each algorithm in the library and combines them based on the model to output the super learner predicted values
+      computePred = function(predY, coef, control, ...) {
+        out <- crossprod(t(predY), coef)
+        return(out)
+      }
+    )
+  } else {
+    stop("Please supply an nlopt or optim method.")
+  }
+  invisible(out)
 }
+
