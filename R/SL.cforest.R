183c51d34f55cfa33be5920d0cef1a022be10d51
Chris Kennedy <chrisken@gmail.com>
2016-07-27 20:36:02 -0700

    Update cforest for binary outcomes; add tests and placeholder man page

--- R/SL.cforest.R
+++ R/SL.cforest.R
@@ -1,12 +1,17 @@
 # cforest {party}
-SL.cforest <- function(Y, X, newX, family, ...){
+# These defaults emulate cforest_unbiased() but allow customization.
+#' TODO: document all parameters.
+SL.cforest <- function(Y, X, newX, family, ntree = 1000,
+                       mtry = max(floor(ncol(X) / 3), 1), mincriterion = 0,
+                       teststat = "quad", testtype = "Univ", replace = F,
+                       fraction = 0.632, ...) {
   .SL.require('party')
-	if(family$family == "gaussian") {
-		fit.cforest <- party::cforest(Y ~ ., data = data.frame(Y, X), controls = party::cforest_unbiased(ntree = 1000, mtry = max(floor(ncol(X)/3), 1)))
-	}
-	if(family$family == "binomial"){
-		stop("Currently only works with gaussian data \ncforest can not return predicted probabilities")
-	}
+	controls = party::cforest_control(ntree = ntree, mtry = mtry,
+	                        mincriterion = mincriterion, teststat = teststat,
+	                        testtype = testtype, replace = replace, fraction = fraction)
+
+	# We can use the same estimation code for classification and regression.
+	fit.cforest <- party::cforest(Y ~ ., data = data.frame(Y, X), controls = controls)
 	pred <- predict(object = fit.cforest, newdata = newX)
 	fit <- list(object = fit.cforest)
 	out <- list(pred = pred, fit = fit)
@@ -14,9 +19,9 @@ SL.cforest <- function(Y, X, newX, family, ...){
 	return(out)
 }
 
-# 
+#
 predict.SL.cforest <- function(object, newdata, ...) {
   .SL.require('party')
 	pred <- predict(object = object$object, newdata = newdata)
-	return(pred) 
+	return(pred)
 }
