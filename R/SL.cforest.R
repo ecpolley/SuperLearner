4830ac01f63a1df2fcda67623fb4374a85f9860a
Chris Kennedy <chrisken@gmail.com>
2016-08-09 08:38:27 -0700

    Add support for observation weights and minimal documentation

--- R/SL.cforest.R
+++ R/SL.cforest.R
@@ -1,7 +1,24 @@
-# cforest {party}
-# These defaults emulate cforest_unbiased() but allow customization.
-#' TODO: document all parameters.
-SL.cforest <- function(Y, X, newX, family, ntree = 1000,
+#' cforest {party}
+#'
+#' These defaults emulate cforest_unbiased() but allow customization.
+#'
+#' @param Y Outcome variable
+#' @param X Covariate dataframe
+#' @param newX Optional dataframe to predict the outcome
+#' @param family "gaussian" for regression, "binomial" for binary
+#'   classification
+#' @param obsWeights Optional observation-level weights (supported but not tested)
+#' @param id Optional id to group observations from the same unit (not used
+#'   currently).
+#' @param ntree Number of trees
+#' @param mtry Number of randomly selected features per node
+#' @param mincriterion See ?cforest_control
+#' @param teststat See ?cforest_control
+#' @param testtype See ?cforest_control
+#' @param replace See ?cforest_control
+#' @param fraction See ?cforest_control
+#' @param ... Remaining arguments (unused)
+SL.cforest <- function(Y, X, newX, family, obsWeights, id, ntree = 1000,
                        mtry = max(floor(ncol(X) / 3), 1), mincriterion = 0,
                        teststat = "quad", testtype = "Univ", replace = F,
                        fraction = 0.632, ...) {
@@ -11,7 +28,8 @@ SL.cforest <- function(Y, X, newX, family, ntree = 1000,
 	                        testtype = testtype, replace = replace, fraction = fraction)
 
 	# We can use the same estimation code for classification and regression.
-	fit.cforest <- party::cforest(Y ~ ., data = data.frame(Y, X), controls = controls)
+	fit.cforest <- party::cforest(Y ~ ., data = data.frame(Y, X), controls = controls,
+	                              weights = obsWeights)
 	pred <- predict(object = fit.cforest, newdata = newX)
 	fit <- list(object = fit.cforest)
 	out <- list(pred = pred, fit = fit)
