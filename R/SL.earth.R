b583a709fbf32e94b025bcc72e6904df191f511a
Eric Polley <ecpolley@gmail.com>
2017-04-21 08:56:07 -0500

    Update SL.earth.R
    add `minspan` and `endspan` parameters

--- R/SL.earth.R
+++ R/SL.earth.R
@@ -1,11 +1,11 @@
 # earth {earth}
-SL.earth <- function(Y, X, newX, family, obsWeights, id, degree = 2, penalty = 3, nk = max(21, 2*ncol(X) + 1), pmethod = "backward", nfold = 0, ncross = 1, ...) {
+SL.earth <- function(Y, X, newX, family, obsWeights, id, degree = 2, penalty = 3, nk = max(21, 2*ncol(X) + 1), pmethod = "backward", nfold = 0, ncross = 1, minspan = 0, endspan = 0,...) {
   .SL.require('earth')
 	if(family$family == "gaussian") {
-		fit.earth <- earth::earth(x = X, y = Y, degree = degree, nk = nk, penalty = penalty, pmethod = pmethod, nfold = nfold, ncross = ncross)
+		fit.earth <- earth::earth(x = X, y = Y, degree = degree, nk = nk, penalty = penalty, pmethod = pmethod, nfold = nfold, ncross = ncross, minspan = minspan, endspan = endspan)
 	}
 	if(family$family == "binomial") {
-		fit.earth <- earth::earth(x = X, y = Y, degree = degree, nk = nk, penalty = penalty, pmethod = pmethod, nfold = nfold, ncross = ncross, glm = list(family = binomial))
+		fit.earth <- earth::earth(x = X, y = Y, degree = degree, nk = nk, penalty = penalty, pmethod = pmethod, nfold = nfold, ncross = ncross, minspan = minspan, endspan = endspan, glm = list(family = binomial))
 	}
 	pred <- predict(fit.earth, newdata = newX, type = "response")
 	fit <- list(object = fit.earth)
