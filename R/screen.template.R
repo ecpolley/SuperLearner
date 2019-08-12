30e4af6ad4169dacd5fbdefac3815e0c77b5e4a1
Eric Polley <ecpolley@gmail.com>
2019-08-12 09:54:10 -0500

    Update screen.template.R

--- R/screen.template.R
+++ R/screen.template.R
@@ -6,7 +6,7 @@ All <- function(X, ...) {
 # screen functions must return a logical vector of length ncol(X)
 screen.template <- function (Y, X, family, obsWeights, id, ...) 
 {
-  # .SL.require('pkg')
+  # library('pkg')
   if (family$family == "gaussian") {
 	
   }
@@ -18,5 +18,5 @@ screen.template <- function (Y, X, family, obsWeights, id, ...)
 }
 
 write.screen.template <- function(file = '', ...) {
-  cat('screen.template <- function(Y, X, family, obsWeights, id, ...) {\n  # load required packages\n  # require(\'pkg\')\n  if (family$family == \'gaussian\') {\n    \n  }\n  if (family$family == \'binomial\') {\n  \n  }\n  # whichVariable is a logical vector,\n  # TRUE indicates variable will be used\n  whichVariable <- rep(TRUE, ncol(X))\n  return(whichVariable)\n}', file = file, ...)
+  cat('screen.template <- function(Y, X, family, obsWeights, id, ...) {\n  # load required packages\n  # library(\'pkg\')\n  if (family$family == \'gaussian\') {\n    \n  }\n  if (family$family == \'binomial\') {\n  \n  }\n  # whichVariable is a logical vector,\n  # TRUE indicates variable will be used\n  whichVariable <- rep(TRUE, ncol(X))\n  return(whichVariable)\n}', file = file, ...)
 }
