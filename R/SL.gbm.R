1b7ce0072b6909363ecb74ec65df0a896585b6ca
Eric Polley <polley.eric@mayo.edu>
2021-01-26 20:03:05 -0600

    Update SL.gbm.R
    added bag.fraction, cv.folds, and n.minobsinnode tuning parameters

--- R/SL.gbm.R
+++ R/SL.gbm.R
@@ -2,17 +2,17 @@
 # generalized boosting algorithm
 # can alter number ot trees in intial fit
 # also alter interaction depth (SL.gbm.1 and SL.gbm.2)
-SL.gbm <- function(Y, X, newX, family, obsWeights, gbm.trees = 10000, interaction.depth = 2, shrinkage = 0.001, ...) {
+SL.gbm <- function(Y, X, newX, family, obsWeights, gbm.trees = 10000, interaction.depth = 2, shrinkage = 0.001, bag.fraction = 0.5, cv.folds = 5, n.minobsinnode = 10, ...) {
   .SL.require('gbm')
   gbm.model <- as.formula(paste("Y~", paste(colnames(X), collapse="+")))
 	if(family$family == "gaussian") {  
-		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "gaussian", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, cv.folds = 5, keep.data = TRUE, weights = obsWeights, verbose = FALSE)
+		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "gaussian", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, bag.fraction = bag.fraction, n.minobsinnode = n.minobsinnode, cv.folds = cv.folds, keep.data = TRUE, weights = obsWeights, verbose = FALSE)
 	}
 	if(family$family == "binomial") {
-		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "bernoulli", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, cv.folds = 5, keep.data = TRUE, weights = obsWeights, verbose = FALSE)
+		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "bernoulli", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, bag.fraction = bag.fraction, n.minobsinnode = n.minobsinnode, cv.folds = cv.folds, keep.data = TRUE, weights = obsWeights, verbose = FALSE)
 	}
 	best.iter <- gbm::gbm.perf(fit.gbm, method = "cv", plot.it = FALSE)
-	pred <- predict(fit.gbm, newdata = newX, best.iter, type = "response")
+	pred <- predict(fit.gbm, newdata = newX, n.trees = best.iter, type = "response")
 	fit <- list(object = fit.gbm, n.trees = best.iter)
 	out <- list(pred = pred, fit = fit)
 	class(out$fit) <- c("SL.gbm")
