3c34a814789de639828e0034e0023ee98319ae69
Eric Polley <ecpolley@gmail.com>
2021-11-29 15:07:27 -0600

    added n.cores to SL.gbm

--- R/SL.gbm.R
+++ R/SL.gbm.R
@@ -2,14 +2,14 @@
 # generalized boosting algorithm
 # can alter number ot trees in intial fit
 # also alter interaction depth (SL.gbm.1 and SL.gbm.2)
-SL.gbm <- function(Y, X, newX, family, obsWeights, gbm.trees = 10000, interaction.depth = 2, shrinkage = 0.001, bag.fraction = 0.5, cv.folds = 5, n.minobsinnode = 10, ...) {
+SL.gbm <- function(Y, X, newX, family, obsWeights, gbm.trees = 10000, interaction.depth = 2, shrinkage = 0.001, bag.fraction = 0.5, cv.folds = 5, n.minobsinnode = 10, n.cores = NULL, ...) {
   .SL.require('gbm')
   gbm.model <- as.formula(paste("Y~", paste(colnames(X), collapse="+")))
 	if(family$family == "gaussian") {  
-		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "gaussian", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, bag.fraction = bag.fraction, n.minobsinnode = n.minobsinnode, cv.folds = cv.folds, keep.data = TRUE, weights = obsWeights, verbose = FALSE)
+		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "gaussian", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, bag.fraction = bag.fraction, n.minobsinnode = n.minobsinnode, cv.folds = cv.folds, keep.data = TRUE, weights = obsWeights, verbose = FALSE, n.cores = n.cores)
 	}
 	if(family$family == "binomial") {
-		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "bernoulli", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, bag.fraction = bag.fraction, n.minobsinnode = n.minobsinnode, cv.folds = cv.folds, keep.data = TRUE, weights = obsWeights, verbose = FALSE)
+		fit.gbm <- gbm::gbm(formula = gbm.model, data = X, distribution = "bernoulli", n.trees = gbm.trees, interaction.depth = interaction.depth, shrinkage = shrinkage, bag.fraction = bag.fraction, n.minobsinnode = n.minobsinnode, cv.folds = cv.folds, keep.data = TRUE, weights = obsWeights, verbose = FALSE, n.cores = n.cores)
 	}
 	best.iter <- gbm::gbm.perf(fit.gbm, method = "cv", plot.it = FALSE)
 	pred <- predict(fit.gbm, newdata = newX, n.trees = best.iter, type = "response")
