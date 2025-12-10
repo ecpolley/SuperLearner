629f143aebabf19751b2c6473263ae769a7be870
Noah Greifer <noah.greifer@gmail.com>
2025-12-10 16:43:25 -0500

    Renamed

--- R/listWrappers.R
+++ R/listWrappers.R
@@ -1,12 +1,36 @@
+#' List all wrapper functions in SuperLearner
+#'
+#' @param what What list to return. Can be \code{both} for both prediction
+#' algorithms and screening algorithms, \code{SL} for the prediction
+#' algorithms, \code{screen} for the screening algorithms, \code{method} for
+#' the estimation method details, or anything else will return a list of all
+#' (exported) functions in the \code{SuperLearner} package. Additional wrapper
+#' functions are available at
+#' \url{https://github.com/ecpolley/SuperLearnerExtra}.
+#' @returns
+#' Invisible character vector with all exported functions in the
+#' \pkg{SuperLearner} package
+#'
+#' @author Eric C Polley \email{epolley@@uchicago.edu}
+#'
+#' @seealso \code{\link{SuperLearner}}
+#'
+#' @keywords utilities
+#' @examples
+#'
+#' listWrappers(what = "SL")
+#' listWrappers(what = "screen")
+
+#' @export
 listWrappers <- function(what = "both") {
 	everything <- sort(getNamespaceExports("SuperLearner"))
-	if(what == "both") {
+	if (what == "both") {
 		message("All prediction algorithm wrappers in SuperLearner:\n")
 		print(everything[grepl(pattern="^[S]L", everything)])
 		message("\nAll screening algorithm wrappers in SuperLearner:\n")
 		print("All")
 		print(everything[grepl(pattern="screen", everything)])
-	} else if(what == "SL") {
+	} else if (what == "SL") {
 		message("All prediction algorithm wrappers in SuperLearner:\n")
 		print(everything[grepl(pattern="^[S]L", everything)])
 	} else if(what == "screen") {
